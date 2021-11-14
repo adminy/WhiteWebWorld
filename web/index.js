9ec1709ed9906f0cd22e6e90620410e2fe4dd532
adminy <marin.bivol@dcu.ie>
2021-11-14 18:47:17 +0000

    (fonts) add fontawesome pro, remove free

--- web/index.js
+++ web/index.js
@@ -1,7 +1,6 @@
 // styles
 import 'bulma/css/bulma.min'
 import 'bulma-tooltip/dist/css/bulma-tooltip.min'
-// import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import './style.scss'
 window.React = require('jsx-dom')
 const choo = require('choo')
